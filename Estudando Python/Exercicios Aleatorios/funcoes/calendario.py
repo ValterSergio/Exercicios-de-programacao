b4f113f5b2c697abd23dc448a422f512c51740a5
Valter Tertuliano <valtertertuliano1994@gmail.com>
2025-10-27 21:51:36 -0300

    Refactor date retrieval in Calendario class

--- Estudando Python/Exercicios Aleatorios/funcoes/calendario.py
+++ Estudando Python/Exercicios Aleatorios/funcoes/calendario.py
@@ -3,18 +3,13 @@ from datetime import datetime
 class Calendario:
     def __init__(self):
         self.__date = datetime.now()
-        self.dia_atual = self.__date.day
-        self.mes_atual = self.__date.month
-        self.ano_atual = self.__date.year
         self.dias_da_semana = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', "Quinta-feira", "Sexta-feira", "Sábado", "Domingo"]
         self.meses_do_ano = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
     def obter_data_atual(self) -> str:
-        dia_int = self.dia_atual
         dia_str = self.dias_da_semana[self.__date.weekday()]
-        mes_int = self.mes_atual
-        mes_str = self.meses_do_ano[mes_int - 1]
-        return f'{dia_str}, {dia_int} de {mes_str} de {self.ano_atual}'
+        mes_str = self.meses_do_ano[self.__date.month - 1]
+        return f'{dia_str}, {self.__date.day} de {mes_str} de {self.__date.year}'
 
 if __name__ == "__main__":
     calendario = Calendario()
